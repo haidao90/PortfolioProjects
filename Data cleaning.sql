d8c7927ae5d7ecd4540f368886d1f9652c4e4703
haidao90 <dao.duyhai90@gmail.com>
2024-09-09 22:38:26 -0400

    Add files via upload

---
+++ Data cleaning.sql
@@ -0,0 +1,149 @@
+
+---  Cleaning data in SQL queries ---
+
+select *
+from PortfolioProject..NashvilleHousing
+
+-- Standardize Date Format
+
+select SaleDateConverted, convert(Date,SaleDate)
+from PortfolioProject..NashvilleHousing
+
+Update NashvilleHousing
+SET SaleDate = convert(Date,SaleDate)
+
+ALTER TABLE NashvilleHousing
+Add SaleDateConverted Date;
+
+Update NashvilleHousing
+SET SaleDateConverted = convert(Date,SaleDate)
+
+---Populate Property Address data
+
+select *
+from PortfolioProject..NashvilleHousing
+--where PropertyAddress is null
+order by ParcelID
+
+
+select a.ParcelID, a.PropertyAddress, b.ParcelID, b.PropertyAddress, ISNULL(a.PropertyAddress,b.PropertyAddress)
+from PortfolioProject..NashvilleHousing a
+join PortfolioProject..NashvilleHousing b
+	on a.ParcelID = b.ParcelID
+	and a.[UniqueID ] <> b.[UniqueID ] 
+where a.PropertyAddress is null
+
+Update a
+SET PropertyAddress = ISNULL(a.PropertyAddress,b.PropertyAddress)
+from PortfolioProject..NashvilleHousing a
+join PortfolioProject..NashvilleHousing b
+	on a.ParcelID = b.ParcelID
+	and a.[UniqueID ] <> b.[UniqueID ] 
+where a.PropertyAddress is null
+
+---- Breaking out Address into Individual Columns (Address, city, State)
+select PropertyAddress
+from PortfolioProject..NashvilleHousing
+
+select
+SUBSTRING (PropertyAddress,1,CHARINDEX(',',PropertyAddress)-1) as Address
+,SUBSTRING (PropertyAddress,CHARINDEX(',',PropertyAddress)+1,LEN(PropertyAddress)) as Address
+from PortfolioProject..NashvilleHousing
+
+ALTER TABLE NashvilleHousing
+Add PropertySplitAddress Nvarchar(255);
+
+Update NashvilleHousing
+SET PropertySplitAddress = SUBSTRING (PropertyAddress,1,CHARINDEX(',',PropertyAddress)-1)
+
+ALTER TABLE NashvilleHousing
+Add PropertySplitCity Nvarchar(255);
+
+Update NashvilleHousing
+SET PropertySplitCity = SUBSTRING (PropertyAddress,CHARINDEX(',',PropertyAddress)+1,LEN(PropertyAddress))
+
+select *
+from PortfolioProject..NashvilleHousing
+
+select OwnerAddress
+from PortfolioProject..NashvilleHousing
+
+---- Another way using PARSENAME for OwnerAddress
+
+Select
+PARSENAME(REPLACE(OwnerAddress, ',','.'),3)
+,PARSENAME(REPLACE(OwnerAddress, ',','.'),2)
+,PARSENAME(REPLACE(OwnerAddress, ',','.'),1)
+from PortfolioProject..NashvilleHousing
+
+ALTER TABLE NashvilleHousing
+Add OwnerSplitAddress Nvarchar(255);
+
+Update NashvilleHousing
+SET OwnerSplitAddress = PARSENAME(REPLACE(OwnerAddress, ',','.'),3)
+
+ALTER TABLE NashvilleHousing
+Add OwnerSplitCity Nvarchar(255);
+
+Update NashvilleHousing
+SET OwnerSplitCity = PARSENAME(REPLACE(OwnerAddress, ',','.'),2)
+
+ALTER TABLE NashvilleHousing
+Add OwnerSplitState Nvarchar(255);
+
+Update NashvilleHousing
+SET OwnerSplitState = PARSENAME(REPLACE(OwnerAddress, ',','.'),1)
+
+select *
+from PortfolioProject..NashvilleHousing
+
+---- Change Y and N to Yes and No in "Sold at Vacant" Field
+
+select distinct (SoldAsVacant), count(SoldAsVacant)
+from PortfolioProject..NashvilleHousing
+group by SoldAsVacant
+order by 2
+
+select SoldAsVacant
+,case when SoldAsVacant = 'Y' then 'Yes'
+	  when SoldAsVacant = 'N' then 'No'
+	  else SoldAsVacant
+	  end 
+from PortfolioProject..NashvilleHousing
+
+
+Update NashvilleHousing
+SET SoldAsVacant = CASE when SoldAsVacant = 'Y' then 'Yes'
+	  when SoldAsVacant = 'N' then 'No'
+	  else SoldAsVacant
+	  end
+
+----Remove Duplicates
+With RowNumCTE AS(
+Select *,
+	ROW_NUMBER() OVER (
+	PARTITION BY ParcelID,
+				PropertyAddress,
+				SalePrice,
+				SaleDate,
+				LegalReference
+				ORDER BY
+					UniqueID
+					) row_num
+from PortfolioProject..NashvilleHousing
+--order by ParcelID
+)
+select *
+from RowNumCTE
+where row_num>1
+order by PropertyAddress
+
+---- Delete Unused Columns
+select *
+from PortfolioProject..NashvilleHousing
+
+Alter Table PortfolioProject..NashvilleHousing
+drop column OwnerAddress, TaxDistrict, PropertyAddress
+
+Alter Table PortfolioProject..NashvilleHousing
+drop column SaleDate
